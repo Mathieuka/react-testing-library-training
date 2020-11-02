bd0954ccfe7aef1e3c7e211c5c2869421968e7de
KAHLAOUI Mathieu <mathieu.kahlaoui.pro@gmail.com>
2020-11-02 09:43:41 +0100

    test prop of component is updated with rerender function

--- src/__tests__/state.js
+++ src/__tests__/state.js
@@ -4,8 +4,14 @@ import user from '@testing-library/user-event'
 import {FavoriteNumber} from '../favorite-number'
 
 test('entering an invalid value shows an error message', () => {
-  const {getByLabelText, getByRole} = render(<FavoriteNumber />)
+  const {getByLabelText, getByRole, rerender, debug} = render(
+    <FavoriteNumber />,
+  )
   const input = getByLabelText(/favorite number/i)
   user.type(input, '10')
   expect(getByRole('alert')).toHaveTextContent(/the number is invalid/i)
+  
+  // we re-render by updating the props of the component 
+  rerender(<FavoriteNumber max={10} />)
+  debug()
 })
