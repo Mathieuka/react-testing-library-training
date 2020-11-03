28e1702a04a7c428e51bc88edc7bf137cf2174b1
KAHLAOUI Mathieu <mathieu.kahlaoui.pro@gmail.com>
2020-11-03 09:36:41 +0100

    assert tha something is not render

--- src/__tests__/state.js
+++ src/__tests__/state.js
@@ -4,14 +4,18 @@ import user from '@testing-library/user-event'
 import {FavoriteNumber} from '../favorite-number'
 
 test('entering an invalid value shows an error message', () => {
-  const {getByLabelText, getByRole, rerender, debug} = render(
+  const {getByLabelText, getByRole, queryByRole, rerender, debug} = render(
     <FavoriteNumber />,
   )
   const input = getByLabelText(/favorite number/i)
   user.type(input, '10')
   expect(getByRole('alert')).toHaveTextContent(/the number is invalid/i)
   
-  // we re-render by updating the props of the component 
+  // we re-render and updating the props of the component 
   rerender(<FavoriteNumber max={10} />)
-  debug()
+
+  // If `role` is not founded `queryByRole` return null but `getByRole` throw an Error
+  // so verify if an element is not render use `queryByRole` instead of `getByRole`
+  expect(queryByRole('alert')).toBeNull()
+
 })
