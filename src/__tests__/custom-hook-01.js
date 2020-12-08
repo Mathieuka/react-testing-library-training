1d81cb781440d524940f68abc5c8bb2805860ead
KAHLAOUI Mathieu <mathieu.kahlaoui.pro@gmail.com>
2020-12-08 20:55:42 +0100

    write a setup function to reduce duplication of testing custom react hooks

--- src/__tests__/custom-hook-01.js
+++ src/__tests__/custom-hook-01.js
@@ -2,16 +2,30 @@ import React from 'react'
 import {render, act} from '@testing-library/react'
 import {useCounter} from '../use-counter'
 
-test('should increment/decrement useCounter Hook', () => {
-  let result
-  function TestComponent() {
-    result = useCounter({initialCount: 0, step: 1})
+function setup({initialProps} = {}) {
+  const result = {current: null}
+  function TestComponent(props) {
+    result.current = useCounter(props)
     return null
   }
-  render(<TestComponent />)
-  expect(result.count).toBe(0)
-  act(() => result.increment())
-  expect(result.count).toBe(1)
-  act(() => result.decrement())
-  expect(result.count).toBe(0)
+  render(<TestComponent {...initialProps} />)
+  return result
+}
+
+test('expose the count and increment/decrement functions', () => {
+  const result = setup()
+  expect(result.current.count).toBe(0)
+  act(() => result.current.increment())
+  expect(result.current.count).toBe(1)
+  act(() => result.current.decrement())
+  expect(result.current.count).toBe(0)
+})
+
+test('expose the count, set step to 2 and increment/decrement', () => {
+  const result = setup({initialProps: {initialCount: 0, step: 2}})
+  expect(result.current.count).toBe(0)
+  act(() => result.current.increment())
+  expect(result.current.count).toBe(2)
+  act(() => result.current.decrement())
+  expect(result.current.count).toBe(0)
 })
