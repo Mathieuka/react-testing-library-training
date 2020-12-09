9a68f3a6ac2a7812bbe4f69eacf545c20fb00c1b
KAHLAOUI Mathieu <mathieu.kahlaoui.pro@gmail.com>
2020-12-09 20:00:03 +0100

    improve reliability of integration Test, by using `user` utility from RTL

--- src/__tests__/app-01.js
+++ src/__tests__/app-01.js
@@ -1,6 +1,7 @@
 /* eslint-disable testing-library/prefer-screen-queries,testing-library/no-debug */
 import React from 'react'
-import {render, fireEvent} from '@testing-library/react'
+import {render} from '@testing-library/react'
+import user from '@testing-library/user-event'
 import App from '../app'
 import {submitForm as mockSubmitForm} from '../api'
 
@@ -10,23 +11,19 @@ test('can fill out a form across multiple pages', async () => {
   mockSubmitForm.mockResolvedValueOnce({success: true})
   const testData = {food: 'test food', drink: 'test drink'}
   const {findByLabelText, findByText} = render(<App />)
-  fireEvent.click(await findByText(/fill.*form/i))
-  fireEvent.change(await findByLabelText(/food/i), {
-    target: {value: testData.food},
-  })
-  fireEvent.click(await findByText(/next/i))
-  fireEvent.change(await findByLabelText(/drink/i), {
-    target: {value: testData.drink},
-  })
-  fireEvent.click(await findByText(/review/i))
+  user.click(await findByText(/fill.*form/i))
+  user.type(await findByLabelText(/food/i), testData.food)
+  user.click(await findByText(/next/i))
+  user.type(await findByLabelText(/drink/i), testData.drink)
+  user.click(await findByText(/review/i))
 
   expect(await findByLabelText(/favorite food/i)).toHaveTextContent('test food')
   expect(await findByLabelText(/favorite drink/i)).toHaveTextContent(
     'test drink',
   )
 
-  fireEvent.click(await findByText(/confirm/i, {selector: 'button'}))
-  fireEvent.click(await findByText(/home/i))
+  user.click(await findByText(/confirm/i, {selector: 'button'}))
+  user.click(await findByText(/home/i))
   expect(mockSubmitForm).toHaveBeenCalledWith(testData)
   expect(mockSubmitForm).toHaveBeenCalledTimes(1)
   expect(await findByText(/welcome home/i)).toBeInTheDocument()
