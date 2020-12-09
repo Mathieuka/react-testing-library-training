a8d18a13ea6a624acb49d8e0fbc85561684f109b
KAHLAOUI Mathieu <mathieu.kahlaoui.pro@gmail.com>
2020-12-09 19:44:16 +0100

    improve reliability of integration Test, by using `find` Query from RTL

--- src/__tests__/app-01.js
+++ src/__tests__/app-01.js
@@ -9,19 +9,25 @@ jest.mock('../api')
 test('can fill out a form across multiple pages', async () => {
   mockSubmitForm.mockResolvedValueOnce({success: true})
   const testData = {food: 'test food', drink: 'test drink'}
-  const {getByLabelText, getByText, findByText} = render(<App />)
-  fireEvent.click(getByText(/fill.*form/i))
-  fireEvent.change(getByLabelText(/food/i), {target: {value: testData.food}})
-  fireEvent.click(getByText(/next/i))
-  fireEvent.change(getByLabelText(/drink/i), {target: {value: testData.drink}})
-  fireEvent.click(getByText(/review/i))
+  const {findByLabelText, findByText} = render(<App />)
+  fireEvent.click(await findByText(/fill.*form/i))
+  fireEvent.change(await findByLabelText(/food/i), {
+    target: {value: testData.food},
+  })
+  fireEvent.click(await findByText(/next/i))
+  fireEvent.change(await findByLabelText(/drink/i), {
+    target: {value: testData.drink},
+  })
+  fireEvent.click(await findByText(/review/i))
 
-  expect(getByLabelText(/favorite food/i)).toHaveTextContent('test food')
-  expect(getByLabelText(/favorite drink/i)).toHaveTextContent('test drink')
+  expect(await findByLabelText(/favorite food/i)).toHaveTextContent('test food')
+  expect(await findByLabelText(/favorite drink/i)).toHaveTextContent(
+    'test drink',
+  )
 
-  fireEvent.click(getByText(/confirm/i, {selector: 'button'}))
+  fireEvent.click(await findByText(/confirm/i, {selector: 'button'}))
   fireEvent.click(await findByText(/home/i))
   expect(mockSubmitForm).toHaveBeenCalledWith(testData)
   expect(mockSubmitForm).toHaveBeenCalledTimes(1)
-  expect(getByText(/welcome home/i)).toBeInTheDocument()
+  expect(await findByText(/welcome home/i)).toBeInTheDocument()
 })
